c89b3d444419d66c170d659bfb80de347ae70304
bertovarian <alberto_bernardez@hotmail.com>
2025-05-27 13:09:39 +0200

    last comming

--- frontend/src/hooks/useShowCamp.js
+++ frontend/src/hooks/useShowCamp.js
@@ -5,7 +5,8 @@ export const useShowCamp = () => {
     const { goError } = errUtils()
     const sendNewCamp = async (id, location) => {
         try {
-            const response = await axios.get(`http://localhost:4000/api/campgrounds/${id}`)
+            // const response = await axios.get(`http://localhost:4000/api/campgrounds/${id}`)
+            const response = await axios.get(`https://yelpcampreact.onrender.com/api/campgrounds/${id}`)
             return response?.data
         } catch (e) {
             goError(e, location)
